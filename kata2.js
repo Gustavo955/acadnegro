3645cfbd8854bab4b81f4b3944d39ad44cf1aeaa
Gustavo <gustavomarquezcortes2@gmail.com>
2022-11-12 09:59:32 -0600

    Nuevo condigo no servia aborto de intento

--- kata2.js
+++ kata2.js
@@ -1,19 +1,12 @@
-function makeHashtag (str) {
-  let wordArray = str.split('').filter(char => char !== "");
-  let result = "#";
-  
-  if (wordArray.length === 0) {
-    return false;
-  };
-  
-  result = result + wordArray.map(word => {
-    let capitalizedWord = word.charAt(0).toUpperCase() + word.slice(1);
-    return capitalizedWord;
-  }).join('');
-  
-  if(result.length > 140) {
-    return false;
-  } else{
-    return result;
-  };
-};
+function generateHashtag(string) {
+  if (string.trim() === '') return false;
+
+  const stringWithCamelCase = string
+    .split(' ')
+    .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+    .join('');
+
+  const stringWithHashtag = `#${stringWithCamelCase.trim()}`;
+
+  return stringWithHashtag.length > 141 ? false : stringWithHashtag;
+}
